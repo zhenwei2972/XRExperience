42edc21a5fa0eef06986f88b55e046bc9a9350ee
Zufiqqar <solomiotrio@gmail.com>
2021-06-23 17:03:08 +0800

    chemical reaction

---
+++ Assets/ChemistryHero/Scripts/Chemical Reaction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChemicalReaction : MonoBehaviour
+{
+
+    public
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void AddChemical(string name)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
