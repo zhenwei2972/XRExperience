b4b7c8dfd7b6d7927d2df7ceecf7fe4ef765ec8a
Zufiqqar <solomiotrio@gmail.com>
2021-06-23 18:28:01 +0800

    Update Chemical Reaction.cs

--- Assets/ChemistryHero/Scripts/Chemical Reaction.cs
+++ Assets/ChemistryHero/Scripts/Chemical Reaction.cs
@@ -8,14 +8,14 @@ public class ChemicalReaction : MonoBehaviour
     List<string> chemical = new List<string>() { 
         "Potassium", "Lithium", "Sulfuric acid", "Nitric acid", //0 1 2 3
         "Potassium Lithium", "Sulfuric Nitric acid", //4 5
-        "Potassium Sulfate", "Potassium Nitrate", //6 7
-        "Lithium Sulfate", "Lithium Nitrate", //8 9
+        "Potassium Sulfate", "Potassium Nitrate",    //6 7
+        "Lithium Sulfate", "Lithium Nitrate",        //8 9
     };
     public int[,] outcomeArr = { 
         { 0, 4, 6, 7   }, //00 01 02 03
-        { 4, 1, 8, 9  }, //10 11 12 13
-        { 6, 8, 2, 5  }, //20 21 22 23
-        { 7, 9, 5, 3  }, //30 31 32 33
+        { 4, 1, 8, 9  },  //10 11 12 13
+        { 6, 8, 2, 5  },  //20 21 22 23
+        { 7, 9, 5, 3  },  //30 31 32 33
     };
 
     // Start is called before the first frame update
